dc089392410d4c9e4ef2fe220c365c07108511ce
Daniel McFarland <danieljmcfarland@gmail.com>
2013-03-20 23:53:11 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,6 +30,8 @@ describe "Authentication" do
   		before { valid_signin(user) }
 
   		it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users', href: users_path) }
   		it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
   		it { should have_link('Sign out', href: signout_path) }
@@ -69,6 +71,11 @@ describe "Authentication" do
           it {should have_selector('title', text: 'Sign in')}
         end
 
+        describe "visiting the users index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
@@ -92,6 +99,18 @@ describe "Authentication" do
         specify { response.should redirect_to(root_path) }
       end 
 
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify {response.should redirect_to(root_path) }
+      end
     end
   end
 end
