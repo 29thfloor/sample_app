0b46cde63731803a378197dcd82fbac5f052571d
Daniel <danieljmcfarland@gmail.com>
2013-04-30 17:25:50 -0400

    Completed section 10.1 - added the Microposts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130321033056) do
+ActiveRecord::Schema.define(:version => 20130430201705) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
